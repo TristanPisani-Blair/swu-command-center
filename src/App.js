eeb973c209c4e5cacb5ca23d4e9546e5b0aa8a18
Tristan Pisani-Blair <tlpisaniblair@student.fullsail.edu>
2024-05-23 07:24:49 -0700

    Still trying to fix errors. Thank you :)

--- src/App.js
+++ src/App.js
@@ -5,20 +5,18 @@ import HowToPlay from './Pages/HowToPlay';
 import Collection from './Pages/Collection';
 import Blogs from './Pages/Blogs';
 import Account from './Pages/Account';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'; // URL router
+import { Route, Routes } from 'react-router-dom'; // URL router
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path="/home" element={<Home/>} />
-        <Route path="/how-to-play" element={<HowToPlay/>} />
-        <Route path="/collection" element={<Collection/>} />
-        <Route path="/blogs" element={<Blogs/>} />
-        <Route path="/account" element={<Account/>} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path="/home" element={<Home/>} />
+      <Route path="/how-to-play" element={<HowToPlay/>} />
+      <Route path="/collection" element={<Collection/>} />
+      <Route path="/blogs" element={<Blogs/>} />
+      <Route path="/account" element={<Account/>} />
+    </Routes>
   );
 }
 

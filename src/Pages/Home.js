456b023779af1aa76bffa945b9d25d2335290a23
krowley011 <KRStevens1@student.fullsail.edu>
2024-05-29 02:50:55 -0500

    Added sort and filter sections with drop downs.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -14,43 +14,40 @@ const Home = () => {
       <div>
         <Navbar />
           <div className="home-content">
+
             <div className="banner-container">
               <img src={spacebkgd} alt="Banner" className="banner" />
               <div className="banner-contents">
                 <div className="banner-contents-left">
                   <h2>Featured Decks</h2>
-                  <h3 className="featured-deck-name">Deck Name</h3>
-                  <p className="featured-deck-publisher">Published by User</p>
+                  <h3>Deck Name</h3>
+                  <p>Published by User</p>
                 </div>
                 <div className="banner-contents-right">
                   <img src={featuredeckimg} alt="Featured Deck" className="featured-deck" />
                 </div>
               </div>
             </div>
+
             <div className="box-container">
               <div className="left-boxes">
-                <Link to="/blogs/news">
-                  <div className="news-box">
+                <Link to="/blogs/news" className="news-box">
                     <h3>News</h3>
                     <img src={bobafett} alt="News" />
-                  </div>
                 </Link>
-                <Link to="/blogs/new">
-                  <div className="blogs-box">
+                <Link to="/blogs/new" className="blogs-box">
                     <img src={chewbacca} alt="Blogs" />
                     <h3>Blogs</h3>
-                  </div>
                 </Link>
               </div>
               <div className="right-box">
-              <Link to="/collection/build-a-deck">
-                <div className="build-a-deck-box">
-                  <h3>Build a Deck</h3>
-                  <img src={darthvader} alt="build-a-deck" />
-                </div>
+              <Link to="/collection" className="collection-box">
+                  <h3>Collection</h3>
+                  <img src={darthvader} alt="Collection" />
               </Link>
               </div>
             </div>
+
           </div>
         <Footer />
       </div>
